cca84bdf9cdf9c288cf659d0d9b56417375b562f
chrisdavery <pchrisdave@gmail.com>
2025-11-24 19:58:00 +0800

    book page

--- assets/custom.js
+++ assets/custom.js
@@ -91,3 +91,31 @@ class ToggleDeposit extends HTMLElement {
 if (!customElements.get('toggle-deposit')) {
   customElements.define('toggle-deposit', ToggleDeposit);
 }
+
+function scrollToHash() {
+  const hash = window.location.hash; // "#faq"
+  if (hash) {
+    const id = hash.slice(1); // remove #
+    const target = document.querySelector(`[data-section-id="${id}"]`);
+    if (target) {
+      let offset = 0;
+      const stickyHeader = document.querySelector('.header[data-sticky-state="active"]');
+      if (stickyHeader) {
+        offset = stickyHeader.clientHeight;
+      }
+
+      const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth"
+      });
+    }
+  }
+}
+
+// Run on page load
+scrollToHash();
+
+// Run on hash change
+window.addEventListener("hashchange", scrollToHash);
